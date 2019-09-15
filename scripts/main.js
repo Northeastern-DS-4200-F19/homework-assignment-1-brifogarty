593231c41431ff7f41cc0270577c4f731b6400b0
Brian Fogarty <fogarty.bri@husky.neu.edu>
2019-09-15 17:57:29 -0400

    hw2

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,24 @@
 // IMAGE CHANGER:
 // assigns the img element to the variable
 let myImage = document.querySelector('img');
+let myTitle = document.querySelector('h1');
 
 // changes the src of myImage depending on the current image
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/firefox-icon.png') {
+  if (mySrc === 'images/firefox-logo.png') {
     myImage.setAttribute('src', 'images/firefox2.png');
+    myTitle.style.color = "#CD3D00";
   } else {
-    myImage.setAttribute('src', 'images/firefox-icon.png');
+    myImage.setAttribute('src', 'images/firefox-logo.png');
+    myTitle.style.color = "#2364F0";
   }
 }
 
 // WELCOME MESSAGE:
 // assigns the button and h1 elements to respective variables
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+let myMessage = document.querySelector('#username');
 
 // prompts the user to enter their name, locally stores the input, and updates the h1 element
 function setUserName() {
@@ -24,7 +27,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myMessage.innerHTML = 'Isnt this is cool, ' + myName + '?';
   }
 }
 
@@ -33,7 +36,7 @@ if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myMessage.textContent = 'Isnt this is cool, ' + storedName + '?';
 }
 
 // assigns the buttons onclick property to the setUserName function
